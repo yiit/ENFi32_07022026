e2b71fe749854dda910b8f57cdb6692922d949b8
Ton Huisman <tonhu2004@hotmail.com>
2025-10-04 21:42:58 +0200

    [Tools] Add Plugin list overview page

--- src/src/CustomBuild/define_plugin_sets.h
+++ src/src/CustomBuild/define_plugin_sets.h
@@ -3834,6 +3834,15 @@ To create/register a plugin, you have to :
   #endif
 #endif // if FEATURE_TASKVALUE_ATTRIBUTES
 
+#ifndef FEATURE_PLUGIN_LIST
+  #ifdef ESP32
+    #define FEATURE_PLUGIN_LIST           1
+  #endif
+  #ifdef ESP8266
+    #define FEATURE_PLUGIN_LIST           0 // Disabled by default on ESP8266
+  #endif
+#endif // ifndef FEATURE_PLUGIN_LIST
+
 //-------------------HTTPResponseParser Section----------------
 #ifndef FEATURE_THINGSPEAK_EVENT
   #if defined(PLUGIN_BUILD_MAX_ESP32)
